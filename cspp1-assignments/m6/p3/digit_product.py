7dd62001a7968c5ab8ddf9715b5f206678e2c347
ktej95 <kteja1989@msitprogram.net>
2018-08-04 11:23:48 +0530

    " -> 2 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignments/m6/p3/digit_product.py
+++ cspp1-assignments/m6/p3/digit_product.py
@@ -6,6 +6,10 @@ else:
     PROD = 1
     while NUM != 0:
         REM = NUM%10
-        PROD = PROD*REM
-        NUM = NUM//10
+        if REM == 0:
+            print(0)
+            break
+        else:
+            PROD = PROD*REM
+            NUM = NUM//10
 print(PROD)
