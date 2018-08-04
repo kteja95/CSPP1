bb50c6e6d9abb3c6516bb5d4ea78c457f3789ea8
ktej95 <kteja1989@msitprogram.net>
2018-08-04 10:43:46 +0530

    " -> 2 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignments/m6/p1/fizz_buzz.py
+++ cspp1-assignments/m6/p1/fizz_buzz.py
@@ -1,12 +1,12 @@
 ''' PROGRAM TO PRINT THE NUMBERS FROM 1 TO N'''
-n=int(input())
-for i in range(1, n+1, 1):
-	if(i%3==0):
-		print("Fizz",end="")
-	elif(i%5==0):
-		print("Buzz",end="")
-	elif(i%3==0 and i%5==0):
-		print("Fizz")
-		print("Buzz")
-	else:
-		print(i,end="")
+NUM = int(input())
+for i in range(1, NUM+1, 1):
+    if i%3 == 0:
+        print("Fizz", end="")
+    elif i%5 == 0:
+        print("Buzz", end="")
+    elif(i%3 == 0 and i% 5 == 0):
+        print("Fizz")
+        print("Buzz")
+    else:
+        print(i, end="")
