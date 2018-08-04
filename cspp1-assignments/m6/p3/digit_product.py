4f894455a49356161f70d2026eba4a872edfbf62
ktej95 <kteja1989@msitprogram.net>
2018-08-04 11:34:33 +0530

    " -> 2 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignments/m6/p3/digit_product.py
+++ cspp1-assignments/m6/p3/digit_product.py
@@ -1,19 +1,17 @@
 '''GIVEN A NUMBER, FIND THE PRODUCT OF THE DIGITS'''
 NUM = int(input())
 COUNT = 0
-if NUM == 0:
+PROD = 1
+while NUM != 0:
+    REM = NUM%10
+    if REM == 0:
+        print(0)
+        COUNT = COUNT+1
+        break
+    else:
+        PROD = PROD*REM
+        NUM = NUM//10
+if COUNT != 0 and REM == 0:
     print(0)
-else:
-    PROD = 1
-    while NUM != 0:
-        REM = NUM%10
-        if REM == 0:
-            print(0)
-            COUNT = COUNT+1
-            break
-        else:
-            PROD = PROD*REM
-            NUM = NUM//10
-if COUNT == 0:
-    print(PROD)
-    
+elif NUM == 0:
+    print(0)
