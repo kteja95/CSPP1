21ed60e7f37abd9d7d3eaf3f6210ebb9adf5171f
ktej95 <kteja1989@msitprogram.net>
2018-08-06 13:58:02 +0530

    hello

---
+++ cspp1-assignments/m7/Functions - Square Exercise/square.py
@@ -0,0 +1,11 @@
+''' THIS PROGRAM RETURNS SQUARE OF THE VALUE'''
+def square(y):
+    y = y**2
+    print(y)
+
+def main():
+    X = int(input())
+    square(X)
+if __name__== "__main__":
+    main()
+
