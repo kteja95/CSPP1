87c694e23c5e8850aeffdaa206a212043d4e842a
ktej95 <kteja1989@msitprogram.net>
2018-08-07 12:40:03 +0530

    assignmnet2m8

---
+++ cspp1-assignments/m8/p2/assignment2.py
@@ -0,0 +1,20 @@
+# Exercise: Assignment-2
+# Write a Python function, sumofdigits, that takes in one number and returns the sum of digits of given number.
+
+# This function takes in one number and returns one number.
+
+
+def sumofdigits(n):
+	if n == 0:
+		return 0
+	else:
+		return n%10 + sumofdigits(n//10)
+    
+
+def main():
+    a = input()
+    print(sumofdigits(int(a)))  
+
+if __name__== "__main__":
+    main()
+
