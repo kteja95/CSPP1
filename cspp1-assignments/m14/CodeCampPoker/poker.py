13fbe71605189f6b40955225402558f713943c61
ktej95 <kteja1989@msitprogram.net>
2018-08-16 12:36:24 +0530

    " -> 16 of 19 passed. pylint: 8.74/10 "

--- cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -88,7 +88,7 @@ def is_threeofakind(hand):
 def is_highcard(hand):
     '''This function implements the high card probability in a hand'''
     xnewlist = sorted(sort(hand))
-    return xnewlist[len(xnewlist)-1]
+    return xnewlist[len(xnewlist)-2]
 
     # high = [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     # differences = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
