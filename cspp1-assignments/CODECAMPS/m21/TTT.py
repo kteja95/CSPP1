55a8e6eca9cb33cdb8b95bfbb4ddda5ca65b86e8
ktej95 <kteja1989@msitprogram.net>
2018-08-24 13:48:07 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- cspp1-assignments/CODECAMPS/m21/TTT.py
+++ cspp1-assignments/CODECAMPS/m21/TTT.py
@@ -9,12 +9,14 @@ def tictactoe(matrix1):
     temp = 0
     new = []
     for i in range(0, len(matrix1), 1):
-        for j in range(len(matrix1[i])):
+        for j in range(len(matrix1[i])-1):
             if matrix1[i][j] == matrix1[i][j+1]:
                 elecount+=1
                 new.append(matrix1[i][j])
                 if elecount == len(matrix1[i])-1:
                     break
+            else:
+                break
 
             elif matrix1[i][j] == matrix1[i+1][j+1] == matrix1[i+2][j+2]:
                 temp+=1
