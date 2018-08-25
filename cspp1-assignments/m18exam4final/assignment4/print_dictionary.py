502956ad0bdb0e6ca1fe0cbf851e46a508bc1a25
ktej95 <kteja1989@msitprogram.net>
2018-08-25 12:44:39 +0530

    " -> 3 of 3 passed. pylint: 8.75/10 "

--- cspp1-assignments/m18exam4final/assignment4/print_dictionary.py
+++ cspp1-assignments/m18exam4final/assignment4/print_dictionary.py
@@ -5,10 +5,12 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
+    '''THIS FUCNTION DOES THE KEYS SORTING'''
     for keys in sorted(dictionary):
-    	print(keys, "-", dictionary[keys])
+        print(keys, "-", dictionary[keys])
 
 def main():
+    '''THIS FUCNTION CALLS THE ABOVE FUCNTION'''
     dictionary = eval(input())
     print_dictionary(dictionary)
 
